16fa9b797d6f6f9300019c0e95e23a47344226c5
LaGretta <sashagutsul2014@gmail.com>
2026-07-16 17:55:42 +0200

    Add Program.cs, JWT setup, initial migration - app runs and DB created

--- MarketPlace.API/Program.cs
+++ MarketPlace.API/Program.cs
@@ -1,23 +1,66 @@
+using System.Text;
+using MarketPlace.Application;
+using MarketPlace.Infrastructure;
+using MarketPlace.Infrastructure.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+var jwtKey = builder.Configuration["Jwt:Key"]!;
+var jwtIssuer = builder.Configuration["Jwt:Issuer"];
+var jwtAudience = builder.Configuration["Jwt:Audience"];
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtIssuer,
+            ValidAudience = jwtAudience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+        };
+    });
+builder.Services.AddAuthorization();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    if (db.Database.IsRelational())
+        await db.Database.MigrateAsync();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();  
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+public partial class Program { }
--- MarketPlace.Infrastructure/DependencyInjection.cs
+++ MarketPlace.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,33 @@
-﻿namespace MarketPlace.Infrastructure;
+﻿using MarketPlace.Application.Interfaces;
+using MarketPlace.Application.Interfaces.Repository;
+using MarketPlace.Application.Interfaces.Security;
+using MarketPlace.Infrastructure.Data;
+using MarketPlace.Infrastructure.Repository;
+using MarketPlace.Infrastructure.Security;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-public class DependencyInjection
+namespace MarketPlace.Infrastructure;
+
+public static  class DependencyInjection
 {
-    
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<IAuthRepository, AuthRepository>();
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<IOrderRepository, OrderRepository>();
+        
+        services.AddScoped<IPasswordHasher, PasswordHasher>();
+        services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+        
+        services.AddScoped<IUnitOfWork, UnitOfWork.UnitOfWork>();
+        
+        return services;
+    }
 }
