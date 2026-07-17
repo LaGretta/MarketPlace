eea00954f3cb2e4236282ebc15b32a64c53ed7b1
LaGretta <sashagutsul2014@gmail.com>
2026-07-17 19:24:26 +0200

    Add unit tests for Category and Auth services

---
+++ MarketPlace.Tests/Services/AuthServiceTests.cs
@@ -0,0 +1,124 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using MarketPlace.Application.DTOs;
+using MarketPlace.Application.Interfaces;
+using MarketPlace.Application.Interfaces.Repository;
+using MarketPlace.Application.Interfaces.Security;
+using MarketPlace.Application.Service;
+using MarketPlace.Domain.Entities;
+using Moq;
+
+namespace MarketPlace.Tests.Services;
+
+public class AuthServiceTests
+{
+    private readonly Mock<IAuthRepository> _authRepositoryMock;
+    private readonly Mock<IJwtTokenGenerator> _jwtTokenGeneratorMock;
+    private readonly Mock<IPasswordHasher> _passwordHasherMock;
+    private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+    private readonly Mock<IMapper> _mapperMock;
+    private readonly AuthService _authService;
+
+    public AuthServiceTests()
+    {
+        _authRepositoryMock = new Mock<IAuthRepository>();
+        _jwtTokenGeneratorMock = new Mock<IJwtTokenGenerator>();
+        _passwordHasherMock = new Mock<IPasswordHasher>();
+        _unitOfWorkMock = new Mock<IUnitOfWork>();
+        _mapperMock = new Mock<IMapper>();
+
+        _authService = new AuthService(
+            _authRepositoryMock.Object,
+            _jwtTokenGeneratorMock.Object,
+            _passwordHasherMock.Object,
+            _unitOfWorkMock.Object,
+            _mapperMock.Object);
+    }
+
+    [Fact]
+    public async Task Register_WhenEmailExists_ThrowsInvalidOperation()
+    {
+        _authRepositoryMock
+            .Setup(r => r.ExistUserByEmail(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(true);
+
+        Func<Task> act = async () =>
+            await _authService.Register(new RegisterDto(), CancellationToken.None);
+
+        await act.Should().ThrowAsync<InvalidOperationException>();
+    }
+
+    [Fact]
+    public async Task Login_WhenUserNotFound_ThrowsUnauthorized()
+    {
+        _authRepositoryMock.Setup(r => r.GetUserByEmail(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((User)null!);
+
+        Func<Task> act = async () => await _authService.Login(new LoginDto(), CancellationToken.None);
+        await act.Should().ThrowAsync<UnauthorizedAccessException>();
+    }
+
+    [Fact]
+    public async Task Login_WhenWrongPassword_ThrowsUnauthorized()
+    {
+        _authRepositoryMock.Setup(r => r.GetUserByEmail(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new User
+            {
+                Id = 1,
+                Email = "sasha21@gmail.com"
+            });
+        _passwordHasherMock.Setup(n => n.Verify(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(false);
+
+        Func<Task> act = async () => await _authService.Login(new LoginDto(), CancellationToken.None);
+        await act.Should().ThrowAsync<UnauthorizedAccessException>();
+    }
+
+    [Fact]
+    public async Task Login_WhenValidCredentials_ReturnsToken()
+    {
+        _authRepositoryMock.Setup(r => r.GetUserByEmail(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new User
+            {
+                Id = 1,
+                Email = "sasha21@gmail.com"
+            });
+        _passwordHasherMock.Setup(n => n.Verify(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(true);
+        _mapperMock.Setup(n => n.Map<AuthResponseDto>(It.IsAny<User>()))
+            .Returns(new AuthResponseDto());
+        _jwtTokenGeneratorMock.Setup(j => j.GenerateJwtToken(It.IsAny<User>())).Returns("token");
+
+        var result = await _authService.Login(new LoginDto(), CancellationToken.None);
+        result.Should().NotBeNull();
+        _authRepositoryMock.Verify(r =>
+            r.GetUserByEmail(It.IsAny<string>(), It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task Register_WhenValidData_CreatesUserAndReturnsToken()
+    {
+        _authRepositoryMock
+            .Setup(r => r.ExistUserByEmail(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(false);
+        _passwordHasherMock
+            .Setup(h => h.Hash(It.IsAny<string>()))
+            .Returns("hashed");
+
+        _mapperMock
+            .Setup(m => m.Map<AuthResponseDto>(It.IsAny<User>()))
+            .Returns(new AuthResponseDto());
+
+        _jwtTokenGeneratorMock
+            .Setup(j => j.GenerateJwtToken(It.IsAny<User>()))
+            .Returns("token");
+        var result = await _authService.Register(new RegisterDto(), CancellationToken.None);
+
+        result.Should().NotBeNull();
+
+        _authRepositoryMock.Verify(r =>
+            r.AddUser(It.IsAny<User>(), It.IsAny<CancellationToken>()), Times.Once);
+        _unitOfWorkMock.Verify(u =>
+            u.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
---
+++ MarketPlace.Tests/Services/CategoryServiceTests.cs
@@ -0,0 +1,123 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using MarketPlace.Application.DTOs;
+using MarketPlace.Application.Interfaces;
+using MarketPlace.Application.Interfaces.Repository;
+using MarketPlace.Application.Service;
+using MarketPlace.Domain.Entities;
+using Moq;
+
+namespace MarketPlace.Tests.Services;
+
+public class CategoryServiceTests
+{
+    private readonly Mock<ICategoryRepository>  _categoryRepositoryMock;
+    private readonly Mock<IMapper>  _mapperMock;
+    private readonly Mock<IUnitOfWork>  _unitOfWorkMock;
+    private readonly CategoryService _categoryService;
+    public CategoryServiceTests()
+    {
+        _categoryRepositoryMock = new Mock<ICategoryRepository>();
+        _mapperMock = new Mock<IMapper>();
+        _unitOfWorkMock = new Mock<IUnitOfWork>();
+        _categoryService = new CategoryService(_categoryRepositoryMock.Object
+            ,_mapperMock.Object
+            , _unitOfWorkMock.Object);
+    }
+    [Fact]
+    public async Task GetCategories_WhenNotFound_KeyNotFoundException()
+    {
+        _categoryRepositoryMock.Setup(n => n.GetCategoryById(It.IsAny<int>(), CancellationToken.None))
+            .ReturnsAsync((Category)null!);
+        
+        Func<Task> act = async () => await _categoryService.GetCategoryById(1,CancellationToken.None);
+        await act.Should().ThrowAsync<KeyNotFoundException>();
+    }
+    [Fact]
+    public async Task GetCategoryById_WhenCategoryExists_ReturnsCategory()
+    {
+        _categoryRepositoryMock
+            .Setup(r => r.GetCategoryById(It.IsAny<int>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new Category());
+        
+        _mapperMock
+            .Setup(m => m.Map<CategoryResponseDto>(It.IsAny<Category>()))
+            .Returns(new CategoryResponseDto());
+
+        var result = await _categoryService.GetCategoryById(1, CancellationToken.None);
+        result.Should().NotBeNull();
+    }
+[Fact]
+    public async Task CreateCategory_WhenValidData_CreatesAndSaves()
+    {
+        _mapperMock
+            .Setup(m => m.Map<Category>(It.IsAny<CreateCategoryDto>()))
+            .Returns(new Category());
+
+        _mapperMock
+            .Setup(m => m.Map<CategoryResponseDto>(It.IsAny<Category>()))
+            .Returns(new CategoryResponseDto());
+
+        var dto = new CreateCategoryDto();
+
+        var result = await _categoryService.CreateCategory(dto, CancellationToken.None);
+
+        result.Should().NotBeNull();
+        _categoryRepositoryMock.Verify(r =>
+            r.CreateCategory(It.IsAny<Category>(), It.IsAny<CancellationToken>()), Times.Once);
+        _unitOfWorkMock.Verify(u =>
+            u.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
+    [Fact]
+    public async Task UpdateCategory_WhenNotFound_ThrowsKeyNotFound()
+    {
+        _categoryRepositoryMock
+            .Setup(r => r.GetCategoryById(It.IsAny<int>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Category)null);
+
+        Func<Task> act = async () =>
+            await _categoryService.UpdateCategory(new UpdateCategoryDto(), 1, CancellationToken.None);
+
+        await act.Should().ThrowAsync<KeyNotFoundException>();
+    }
+    [Fact]
+    public async Task UpdateCategory_WhenValidData_UpdatesAndSaves()
+    {
+        _categoryRepositoryMock
+            .Setup(r => r.GetCategoryById(It.IsAny<int>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new Category());
+
+        await _categoryService.UpdateCategory(new UpdateCategoryDto(), 1, CancellationToken.None);
+
+        _categoryRepositoryMock.Verify(r =>
+            r.UpdateCategory(It.IsAny<Category>()), Times.Once);
+        _unitOfWorkMock.Verify(u =>
+            u.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
+    [Fact]
+    public async Task DeleteCategoryById_WhenNotFound_ThrowsKeyNotFound()
+    {
+        _categoryRepositoryMock
+            .Setup(r => r.GetCategoryById(It.IsAny<int>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Category)null);
+
+        Func<Task> act = async () =>
+            await _categoryService.DeleteCategoryById(1, CancellationToken.None);
+
+        await act.Should().ThrowAsync<KeyNotFoundException>();
+    }
+    [Fact]
+    public async Task DeleteCategoryById_WhenValidData_DeletesAndSaves()
+    {
+        _categoryRepositoryMock
+            .Setup(r => r.GetCategoryById(It.IsAny<int>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new Category());
+
+        await _categoryService.DeleteCategoryById(1, CancellationToken.None);
+
+        _categoryRepositoryMock.Verify(r =>
+            r.DeleteCategory(It.IsAny<Category>()), Times.Once);
+        _unitOfWorkMock.Verify(u =>
+            u.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
